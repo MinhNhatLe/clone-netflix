1aac4a601c9af331cd3e5f62a42a5c985c0f1505
nhat <symbol057vn@gmail.com>
2023-03-30 11:16:08 +0700

    add

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import React from "react";
+
+const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
